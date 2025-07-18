cd017600d754e6d1216f758e04df40a6a8ebe834
dewstouh <diego@justdiego.com>
2025-07-18 16:34:55 +0200

    feat(web): wrap layout with auth provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { AuthProvider } from "../components/auth-provider";
+import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
